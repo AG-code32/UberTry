71f868b595ddc670dcdbf9dad974fe24657f465a
AG-code32 <125658213+AG-code32@users.noreply.github.com>
2024-09-11 04:57:45 +1000

    Email Authentication, Bottom Tab Bar

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -32,7 +32,7 @@ const InputField = ({
             {label}
           </Text>
           <View
-            className={`flex flex-row justify-start items-center relative bg-sky-700 rounded-full border border-neutral-100 focus:border-primary-500 ${containerStyle} text-blue-600/100`}
+            className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-primary-500  ${containerStyle}`}
           >
             {icon && (
               <Image source={icon} className={`w-6 h-6 ml-4 ${iconStyle}`} />
@@ -40,6 +40,7 @@ const InputField = ({
             <TextInput
               className={`rounded-full p-4 font-JakartaSemiBold text-[15px] flex-1 ${inputStyle} text-left`}
               secureTextEntry={secureTextEntry}
+              placeholderTextColor="#c6c6c6"
               {...props}
             />
           </View>
